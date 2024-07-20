293b232228647fc47a30609b3744ee2aed6005ae
ssk090 <shivanandasai.38@gmail.com>
2024-07-21 01:08:20 +0530

    feat: Processor checkpoint

---
+++ hooks/prisma/migrations/20240720193636_remove_sorting_order/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `sortingOrder` on the `Trigger` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Trigger" DROP COLUMN "sortingOrder";
